5b02b676a82b70fa53959451c91bfa14c3a978a9
pvestal <Patrick.vestal@gmail.com>
2019-09-13 12:27:15 -0400

    updates

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -24,7 +24,7 @@
                 </v-card-title>
                 <v-card-actions>
                   <v-btn flat :to="'/meetups/' + meetup.id">
-                    <v-icon left light>arrow_forward<v-icon>
+                    <v-icon left light>arrow_forward</v-icon>
                   View Meetup
                   </v-btn>
                 </v-card-actions>
